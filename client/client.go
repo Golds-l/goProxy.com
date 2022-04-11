7f7f58b5bf81909c710025c659d0dde238cc2b81
Golds <goldslee@foxmail.com>
2022-04-11 22:51:34 +0800

    rebuild file tree

--- client/client.go
+++ client/client.go
@@ -3,16 +3,37 @@ package client
 import (
 	"fmt"
 	"net"
+	"os"
 
+	"github.com/Golds-l/goproxy/communication"
 	"github.com/Golds-l/goproxy/other"
 )
 
-func MakeNewClient(serverAddr, localAddr string) (net.Conn, net.Conn) {
-	connServer, err := net.Dial("tcp", serverAddr)
-	other.HandleErr(err)
-	fmt.Println("make new client")
-	other.HandleErr(err)
-	connLocal, connLOcalErr := net.Dial("tcp", localAddr)
-	other.HandleErr(connLOcalErr)
-	return connServer, connLocal
+func main() {
+	argsMap, ok := other.GetArgsRemoteClient()
+	fmt.Printf("server:%v ", argsMap["CloudServer"]+":"+argsMap["cloudServerPort"])
+	fmt.Printf("host:%v\n", argsMap["remoteHost"]+":"+argsMap["remoteHostPort"])
+	if !ok {
+		fmt.Println("args illegal")
+		os.Exit(0)
+	}
+	addrCloud := argsMap["CloudServer"] + ":" + argsMap["cloudServerPort"]
+	addrLocal := argsMap["remoteHost"] + ":" + argsMap["remoteHostPort"]
+	communicationConn, communicationConnErr := net.Dial("tcp", addrCloud)
+	other.HandleErr(communicationConnErr)
+	communicationConn.Write([]byte("RCReady"))
+	fmt.Println("client ready")
+	cache := make([]byte, 10240)
+	for {
+		n, readErr := communicationConn.Read(cache)
+		if readErr != nil {
+			fmt.Printf("%v\n", readErr)
+			continue
+		}
+		if string(cache[:n]) == "NEWXX" {
+			connCloud, connLocal := MakeNewClient(addrCloud, addrLocal)
+			go communication.RemoteClientToCloudServer(connCloud, connLocal)
+			go communication.CloudServerToRemoteClient(connCloud, connLocal)
+		}
+	}
 }
--- server/server.go
+++ server/server.go
@@ -3,16 +3,40 @@ package server
 import (
 	"fmt"
 	"net"
+	"os"
+
+	"github.com/Golds-l/goproxy/communication"
+	"github.com/Golds-l/goproxy/other"
 )
 
-func MakeNewConn(communicationConn net.Conn, listener net.Listener) net.Conn {
-	_, err := communicationConn.Write([]byte("NEWXX")) // make new connection
+func main() {
+	argsMap, ok := other.GetArgsCloudServer()
+	if !ok { // need update
+		fmt.Println("args error")
+		os.Exit(0)
+	}
+	listenLocal, err := net.Listen("tcp", ":"+argsMap["localPort"])
 	if err != nil {
-		fmt.Printf("client connection error. %v\n", err)
+		fmt.Println("listen err", err)
 	}
-	newConn, newConnectionErr := listener.Accept()
-	if newConnectionErr != nil {
-		fmt.Printf("connection made error. %v\n", err)
+	listenRemote, err := net.Listen("tcp", ":"+argsMap["remotePort"])
+	if err != nil {
+		fmt.Println("listen err", err)
 	}
-	return newConn
+	fmt.Printf("begin listen... local port:%v remote port:%v\n", argsMap["localPort"], argsMap["remotePort"])
+	communicationConn, communicationConnErr := listenRemote.Accept()
+	other.HandleErr(communicationConnErr)
+	ACKCache := make([]byte, 1024)
+	n, _ := communicationConn.Read(ACKCache)
+	if string(ACKCache[:n]) == "RCReady" {
+		fmt.Println("cloud server --- remote client is connected!")
+	}
+	for {
+		connLocal, connLocalErr := listenLocal.Accept()
+		fmt.Printf("connect from %v\n", connLocal.RemoteAddr())
+		other.HandleErr(connLocalErr)
+		connRemote := MakeNewConn(communicationConn, listenRemote)
+		go communication.CloudServerToLocal(connRemote, connLocal)
+		go communication.LocalToCloudServer(connRemote, connLocal)
+	}
 }
