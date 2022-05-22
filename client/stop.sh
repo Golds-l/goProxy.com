25570909121c6e0f1094eaad605c54806ac7b2c6
Golds-l <3365638834@qq.com>
2022-05-22 23:30:40 +0800

    up stop.sh

--- client/stop.sh
+++ client/stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pid=`cat /run/goproxy.pid`
-logpath="/home/golds/goproxy/proxy.log"
+logpath="/path/to/goproxy/proxy.log"
 kill -9 $pid
 echo "stop: $(date "+%Y-%m-%d %H:%M:%S")" >> proxy.log
